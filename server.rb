12aefcc71a0a18b383065739b53569c71f08dd59
Jan van der Pas <janvanderpas@gmail.com>
2016-11-03 22:09:21 +0100

    Mitigate some of the security risk. Since we can't use the safe level

--- server.rb
+++ server.rb
@@ -1,11 +1,39 @@
+# frozen_string_literal: true
 require 'drb'
 require 'bundler'
 Bundler.require(:default, :development, :server)
 
-# TODO: Ensure security!
+# Security notice!
+#
+# Normally you would run the DRb server with $SAFE=1,
+# to update the safe level. This prevents untrusted calls
+# to the system, e.g. disables eval() and friends.
+#
+# However since WickedPdf uses Open3 to pass the
+# body to wkhtml2pdf, we can't set the safe level. Otherwise
+# the client will fail with DRb::DRbConnError.
+#
+# Instead of using the safe level we try to mitigate some
+# of the security risks by undefining 'dangerous' methods.
+#
+# Methods __send__() and send() can't be undefined.
+#
+# More information:
+# https://ruby-doc.org/stdlib-2.3.0/libdoc/drb/rdoc/DRb.html#module-DRb-label-Security
+# http://blog.recurity-labs.com/archives/2011/05/12/druby_for_penetration_testers/
 
-# Disable eval() and friends
-# $SAFE = 1
+# Kernel methods:
+undef :`
+undef :eval
+undef :exec
+undef :syscall
+undef :system
+# Object methods
+undef :untaint
+undef :untrust
+# BasicObject methods:
+undef :instance_eval
+undef :instance_exec
 
 # DRB Server URI
 if ENV['DOCKER']
@@ -34,13 +62,14 @@ WickedPdf.config = {
 
 class PDFPrinterServer
   def print(body)
-    WickedPdf.new.pdf_from_string(body, page_size: 'A4')
+    tainted_body = body.taint
+
+    WickedPdf.new.pdf_from_string(tainted_body, page_size: 'A4')
   end
 end
 
 pdf_printer_server = PDFPrinterServer.new
-
 $stdout.puts('Started server...')
-DRb.start_service(SERVER_URI, pdf_printer_server)
+DRb.start_service(SERVER_URI, pdf_printer_server, verbose: true)
 DRb.thread.join # Wait for the drb server thread to finish before exiting.
 $stdout.puts('Stopped server!')
